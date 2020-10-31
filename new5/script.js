08b08eeffe290400932b0c5e9538a6193278fb42
“mmoralesross” <mmoralesross@gmail.com>
2020-10-31 17:22:12 -0400

    working on changing colors

---
+++ new5/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+//set time
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    var currentHour = moment().hour();
+
+
+// change colors
+$('.time-block').each(function(){
+    var id = parseInt($(this).attr('id'));
+    if(id < currentHour){
+        $(this).addClass('past')
+    } else if( id === currentHour){
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+})
+
+
+
